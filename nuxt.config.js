a24ffca82d84eb969e65f59db8b7cb2621d66e24
Houssam Benhim <benhim.houssam@gmail.com>
2021-10-05 21:49:47 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+export default {
+  components: true,
+  head: {
+    titleTemplate: "PsySelect",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      {
+        charset: "utf-8",
+      },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "psyselect app",
+      },
+    ],
+  },
+  modules: ["~/modules/test"],
+};
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float:left;margin:10px"
+    >
+      <home-card :home="home"></home-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from "~/data/homes";
+export default {
+  head() {
+    return {
+      meta: [
+        {
+          name: "psyselect app",
+          content: "homepage",
+          hid: "description",
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
